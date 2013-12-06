e69bd84dcd5d5a07ee63fd64ccb1b8180185e305
mjwade <michael.j.wade@intel.com>
2013-12-06 14:24:38 -0800

    changed the curl options on the files to get rid of the header junk.

--- webinstall.sh
+++ webinstall.sh
@@ -7,7 +7,7 @@ set -u
 # Installation will put two files in place under $INSTALLDIR
 # syntax/simics.vim == highlighting
 # ftdetect/simics.vim == filetype detection
-SOURCE=https://raw.github.com/mjwade/vim-syntax-simics/tree/master
+SOURCE=https://raw.github.com/mjwade/vim-syntax-simics/master
 INSTALLDIR=$HOME/.vim
 
 echo "Thanks for installing!"
@@ -19,13 +19,13 @@ echo "Press enter to continue.  Ctrl+c to stop"
 read
 
 # Create the needed paths and files
-mkdir -p $INSTALLDIR/syntax/simics.vim 
+mkdir -p $INSTALLDIR/syntax/
 if [ $? -ne 0 ]; then #failed
     EXITCODE=$?
     echo "failed to mkdir -p on $INSTALLDIR/syntax/simics.vim"
     exit $EXITCODE
 fi
-mkdir -p $INSTALLDIR/ftdetect/simics.vim
+mkdir -p $INSTALLDIR/ftdetect/
 if [ $? -ne 0 ]; then #failed
     EXITCODE=$?
     echo "failed to mkdir -p on $INSTALLDIR/ftdetect/simics.vim"
@@ -35,8 +35,8 @@ fi
 # Fill the files 
 # NOTE: Assumes you're running this from the web...
 echo "Fetching the files simics files from the web."
-curl -silent $SOURCE/syntax/simics.vim > $INSTALLDIR/syntax/simics.vim
-curl -silent $SOURCE/ftdetect/simics.vim > $INSTALLDIR/ftdetect/simics.vim
+curl --raw --silent $SOURCE/syntax/simics.vim > $INSTALLDIR/syntax/simics.vim
+curl --raw --silent $SOURCE/ftdetect/simics.vim > $INSTALLDIR/ftdetect/simics.vim
 
 # Stuff should be in the files now...
 echo "Installation complete."
